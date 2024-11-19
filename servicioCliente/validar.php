86f44285bce13bcb9a5be15bc77d8b33f35d5806
maria <marieta_nena@hotmail.com>
2024-11-19 20:55:46 +0100

    creacion servicio validar cliente

--- servicioCliente/validar.php
+++ servicioCliente/validar.php
@@ -3,51 +3,21 @@ session_start();
 include __DIR__ . "/config/autoload.php";
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $pass = $_POST['pwd'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $dniCliente = $_GET['dniCliente'];
+    $pwd = $_GET['pwd'];
 
     $base = new Base();
-    $cli = new Cliente(); 
+    $cli = new Cliente($dniCliente, "", "","",$pwd); 
 
-    try {
         // Validar cliente
-        $resultado = $cli->validar($email, $pass);
+        $resultado = $cli->validar($base->link);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($resultado);
+        exit();
 
-
-        if ($resultado) {
-            // Crear sesiones
-            $_SESSION['email'] = $resultado['email'];
-            $_SESSION['pwd'] = $resultado['pwd'];
-            $_SESSION['nombre'] = $resultado['nombre'];
-           
-            // Comprobar si existe la variable de sesión idUnico
-            if (!isset($_SESSION['idUnico'])) {
-                $_SESSION['idUnico'] = uniqid();
-                $idUnico = $_SESSION['idUnico'];
-                $nombre = $_SESSION['nombre'];
-                
-            }
-             // suludar al usuario
-             $mensaje = "Hola, " . $_SESSION['nombre'];
-             echo "<script>alert('$mensaje');</script>";
-
-            // Redirigir a inicio
-            header("Location:../vistas/index2.html");
-            exit();
-            
-        } else {
-           echo "el cliente no existe. Debes registrarte";
-            header("Location: ../vistas/registro.html");
-            exit();
-           
-        }
-    } catch (Exception $e) {
-        echo "¡Error!: " . $e->getMessage();
-        die();
-    }
 } else {
-    echo "Error: vuelva a intentarlo";
+    echo json_encode( "Error: vuelva a intentarlo");
 }
 
 
