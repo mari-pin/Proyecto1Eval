2dbc27e61bb368d4cb1e5d98d14b31fb7b2269fc
maria <marieta_nena@hotmail.com>
2024-11-20 19:31:32 +0100

    principal ya funciona todo correcto

--- controladores/principal.php
+++ controladores/principal.php
@@ -5,36 +5,41 @@ include "../vistas/iniParcialProductos.html";
 
 $base = new Base();
 
-$resultado = json_decode(file_get_contents("http://localhost/Proyecto/servicioProducto/producto.php"),true);
+//obtengo los datos del servico
+$resultado= json_decode(file_get_contents("http://localhost/Proyecto/servicoProducto/producto.php"),true);
+ 
 
-include "../vistas/productos.html";
-include "../vistas/finParcialProductos";
+ /* include "../vistas/productos.html";  */
 
 
-/* falta introducir todo el foreach de cada producto */
-/*   <section id="productos" class="py-5">
-    <div class="container-fluid">
-      <h2 class="text-center mb-5 titulo">Nuestros Bombones</h2>
-      <div class="row justify-content-center">
-        <?php foreach ($productos as $producto): ?>
-        <div class="col-12 col-sm-8 col-md-6 col-lg-3 d-flex justify-content-center mb-4">
-          <div class="card product-card">
-            <img src="<?php echo htmlspecialchars($producto['imagen']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($producto['nombre']); ?>" />
-            <div class="card-body text-center">
-              <h5 class="card-title"><?php echo htmlspecialchars($producto['nombre']); ?></h5>
-              <p class="card-text"><?php echo htmlspecialchars($producto['descripcion']); ?></p>
-              <p class="card-text">$<?php echo htmlspecialchars($producto['precio']); ?></p>
-              <button class="btn btn-detalle">Comprar
-                <!-- Icono cart -->
-                <a href="/vistas/carrito.html">
-                  <i class="fa-solid fa-cart-shopping mt-2"></i>
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-        <?php endforeach; ?>
-      </div>
-    </div>
-  </section> */
 
+
+// Productos Destacados
+    echo '<section id="productos" class="py-5">';
+    echo '<div class="container-fluid">';
+     echo '<h2 class="text-center mb-5 titulo">Nuestros Bombones</h2>';
+     echo'<div class="row justify-content-center">';
+   
+    foreach ($resultado as $producto) {
+
+    
+      echo '<div class="col-12 col-sm-8 col-md-6 col-lg-3 d-flex justify-content-center mb-4">';
+      echo '<div class="card product-card">';
+      echo '<img src="' . htmlspecialchars($producto['foto']) . '" class="card-img-top" alt="" />';
+      echo '<div class="card-body text-center">';
+      echo '<h5 class="card-title">' . htmlspecialchars($producto['nombre']) . '</h5>';
+      echo '<button class="btn btn-detalle">Comprar';
+      echo '<a href="/vistas/carrito.html">';
+      echo '<i class="fa-solid fa-cart-shopping mt-2"></i>';
+      echo '</a>';
+      echo '</button>';
+      echo '</div>';
+      echo '</div>';
+      echo '</div>';
+  }
+  
+    echo ' </div>';
+   echo '</div>';
+  echo'</section>';
+  
+  include "../vistas/finParcialProductos.html";
