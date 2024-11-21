28d3953d77e4e992c21859a5ce0a2c003011ebe7
maria <marieta_nena@hotmail.com>
2024-11-21 20:48:01 +0100

    cambios del los archivos remotos

--- controladores/carrito.php
+++ controladores/carrito.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 require "../config/autoload.php";
@@ -13,19 +12,4 @@ include "../vistas/carrito.html";
 
 
 include "../vistas/finParcialProductos.html";
-=======
-<?php
-session_start();
-require "../config/autoload.php";
-include "../vistas/iniParcialProductos.html";
 
-
-
-
-
-include "../vistas/carrito.html";
-
-
-
-include "../vistas/finParcialProductos.html";
->>>>>>> 2da4c6c02739034c040714be351d5dd8cdf17326
--- controladores/principal.php
+++ controladores/principal.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 require "../config/autoload.php";
@@ -44,51 +43,4 @@ $resultado= json_decode(file_get_contents("http://localhost/Proyecto/servicoProd
    echo '</div>';
   echo'</section>';
   
-=======
-<?php
-session_start();
-require "../config/autoload.php";
-include "../vistas/iniParcialProductos.html";
-
-$base = new Base();
-
-//obtengo los datos del servico
-$resultado= json_decode(file_get_contents("http://localhost/Proyecto/servicoProducto/producto.php"),true);
- 
-
- /* include "../vistas/productos.html";  */
-
-
-
-
-// Productos Destacados
-    echo '<section id="productos" class="py-5">';
-    echo '<div class="container-fluid">';
-     echo '<h2 class="text-center mb-5 titulo">Nuestros Bombones</h2>';
-     echo'<div class="row justify-content-center">';
-   
-    foreach ($resultado as $producto) {
-
-    
-      echo '<div class="col-12 col-sm-8 col-md-6 col-lg-3 d-flex justify-content-center mb-4">';
-      echo '<div class="card product-card">';
-      echo '<img src="' . htmlspecialchars($producto['foto']) . '" class="card-img-top" alt="" />';
-      echo '<div class="card-body text-center">';
-      echo '<a href="detalle.php">ver detalle</a>';
-      echo '<h5 class="card-title">' . htmlspecialchars($producto['nombre']) . '</h5>';
-      echo '<button class="btn btn-detalle">Comprar';
-      echo '<a href="/vistas/carrito.html">';
-      echo '<i class="fa-solid fa-cart-shopping mt-2"></i>';
-      echo '</a>';
-      echo '</button>';
-      echo '</div>';
-      echo '</div>';
-      echo '</div>';
-  }
-  
-    echo ' </div>';
-   echo '</div>';
-  echo'</section>';
-  
->>>>>>> 2da4c6c02739034c040714be351d5dd8cdf17326
   include "../vistas/finParcialProductos.html";
