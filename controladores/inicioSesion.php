7d14c170bb51f217bbb2449f895afd1926bba0aa
maria <marieta_nena@hotmail.com>
2024-11-22 01:17:19 +0100

    inicio sesion creado y funciona

--- controladores/inicioSesion.php
+++ controladores/inicioSesion.php
@@ -6,15 +6,42 @@ include "../vistas/vistParcialFormulario.html";
 $base = new Base();
 
 if(isset($_POST['enviar'])){
-    $cli= new Cliente("dniCliente", "", "", "","pwd");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $dniCliente = $_POST['dniCliente'];
+        $pwd = $_POST['pwd'];
     
-    var_dump($_POST['dniCliente'], $_POST['pwd']);
+        // Realizar la petición al servicio de validación
+        $url = "http://localhost/Proyecto/servicioCliente/validar.php?dniCliente=" . urlencode($dniCliente) . "&pwd=" . urlencode($pwd);
+        $resultado = json_decode(file_get_contents($url), true);
+    
+        // Verificar si la respuesta es válida
+        if ($resultado && !isset($resultado['error'])) {
+            // Crear sesiones
+            $_SESSION['email'] = $resultado['email'];
+            $_SESSION['pwd'] = $resultado['pwd'];
+            $_SESSION['nombre'] = $resultado['nombre'];
+    
+            // Redirigir a inicio
+            header("Location:principal.php");
+            exit();
+        } else {
+            echo "<p>Dni y contraseña incorrectos. Debes registrarte <a href='registro.php'>Registrarse</a></p>";
+        }
+    }
+    include "principal.php";
+
+  /*   $cli= new Cliente($_POST['dniCliente'], '','', '',$_POST['pwd']);
+    
+   
+    var_dump($_POST['dniCliente'], $_POST['pwd'], $_POST['nombre']);
     $cli->buscar($base->link);
-    $_SESSION['nombre'] = $_POST['nombre'];
+  
 
     $resultado = json_decode(file_get_contents("http://localhost/Proyecto/servicioCliente/validar.php?dniCliente=".$_POST['dniCliente']."&pwd=".$_POST['pwd']),true);
-
+    $_SESSION['nombre'] = $_POST['nombre'];
 var_dump($resultado);
+
     if (isset($_POST['dniCliente'], $_POST['pwd'])) {
         echo "entro en el if";
         // Crear sesiones
@@ -32,7 +59,7 @@ var_dump($resultado);
       
        
        
-    }
+    } */
 }else{
     include "../vistas/inicioSesion.html";
 
