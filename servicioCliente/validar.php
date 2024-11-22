416ca96520a1a400d5a68627772327646312c282
maria <marieta_nena@hotmail.com>
2024-11-22 01:20:11 +0100

    modificados fallos

--- servicioCliente/validar.php
+++ servicioCliente/validar.php
@@ -1,21 +1,27 @@
-<<<<<<< HEAD
 <?php
 session_start();
-include __DIR__ . "/config/autoload.php";
+include  "config/autoload.php";
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-var_dump($_GET);
+
 if(isset($_GET['dniCliente'],$_GET['pwd'])){
 
     $dniCliente = $_GET['dniCliente'];
     $pwd = $_GET['pwd'];
     $base = new Base();
     $cli = new Cliente($dniCliente, "", "","",$pwd); 
+    if($cli){
         // Validar cliente
         $resultado = $cli->validar($base->link);
+        $_SESSION['nombre'] = $resultado['nombre'];
         header("HTTP/1.1 200 OK");
         echo json_encode($resultado);
+
+    }else{
+        echo "sus dato no estan registrados";
+    }
+        
         exit();
 
   
@@ -31,37 +37,3 @@ if(isset($_GET['dniCliente'],$_GET['pwd'])){
 }
 
 
-=======
-<?php
-session_start();
-include __DIR__ . "/config/autoload.php";
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-var_dump($_GET);
-if(isset($_GET['dniCliente'],$_GET['pwd'])){
-
-    $dniCliente = $_GET['dniCliente'];
-    $pwd = $_GET['pwd'];
-    $base = new Base();
-    $cli = new Cliente($dniCliente, "", "","",$pwd); 
-        // Validar cliente
-        $resultado = $cli->validar($base->link);
-        header("HTTP/1.1 200 OK");
-        echo json_encode($resultado);
-        exit();
-
-  
-}else{
-    echo "error:faltan datos";
-}
-
-
-    
-
-} else {
-    echo json_encode( "Error: vuelva a intentarlo");
-}
-
-
->>>>>>> 2da4c6c02739034c040714be351d5dd8cdf17326
